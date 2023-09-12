1676f677e9102e1a3c7e188c57e14198ad9dd257
Johanna Geßner <johanna-gessner@web.de>
2023-09-12 16:43:34 +0200

    examples of conditional rendering

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,6 @@ function Main(){
     const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
 
     const deleteMovie = (movieId) => {
-        console.log("deleting on movie with id....", movieId);
 
         const newList = moviesToDisplay.filter( (element) => {
             if(element.id !== movieId){
@@ -18,25 +17,45 @@ function Main(){
             } else {
                 return false;
             }
-            
         });
+
+
+        // const newList = moviesToDisplay.filter( (element) => {
+        //     return element.id !== movieId;
+        // });
+
 
         setMoviesToDisplay(newList);
 
     }
 
+    let message;
+    if(moviesToDisplay.length > 0){
+        message = <h1>Number of movies: {moviesToDisplay.length}</h1>
+    } else {
+        message = <h1>Sorry, no movies to display</h1>
+    }
+
 
     return (
         <div className="Main">
-            <h1>Number of movies: {moviesToDisplay.length}</h1>
+
+            {message}
+            
 
             {moviesToDisplay.map((movieObj) => {
                 return (
                     <section key={movieObj.id} className="card">
-                        <img src={movieObj.imgURL} alt={movieObj.title} />
                         <h2>{movieObj.title}</h2>
+
+                        {movieObj.imgURL 
+                            ? <img src={movieObj.imgURL} alt={movieObj.title} />
+                            : <img src="https://dummyimage.com/182x268/ffffff/000000" />
+                        }
+                       
                         <h3>Year: {movieObj.year}</h3>
                         <h4>Rating:{movieObj.rating}</h4>
+                        {movieObj.rating > 8 && <p>RECOMMENDED</p>}
 
                         <button onClick={() => {deleteMovie(movieObj.id)}}>Delete</button>
                     </section>
