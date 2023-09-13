24e4c25496ed0942bef915fffbc90e1273a7d899
Johanna Geßner <johanna-gessner@web.de>
2023-09-13 15:54:11 +0200

    extract form to AddMovie component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import movies from "./data/movies.json";
 
 import Header from './components/Header'
+import AddMovie from "./components/AddMovie";
 import Main from './components/Main'
 import Footer from './components/Footer'
 
@@ -12,81 +13,25 @@ function App() {
 
   const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
 
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState("");
-  const [imgURL, setImage] = useState("");
-  
-
   const deleteMovie = (movieTitle) => {
-    const newList = moviesToDisplay.filter( (element) => {
-        return element.title !== movieTitle;
+    const newList = moviesToDisplay.filter((element) => {
+      return element.title !== movieTitle;
     });
     setMoviesToDisplay(newList);
   }
 
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    const newMovie = {
-      title: title,
-      rating: rating,
-      imgURL: imgURL
-    }
 
+  const addNewMovie = (newMovie) => {
     const newList = [newMovie, ...moviesToDisplay];
-    
     setMoviesToDisplay(newList);
-
-    // clear form
-    setTitle("");
-    setRating("");
-    setImage("");
   }
+
 
   return (
     <>
       <Header numberOfMovies={moviesToDisplay.length} />
-
-      <section>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Title:
-            <input 
-              type="text" 
-              name="title" 
-              placeholder="enter the title" 
-              value={title} 
-              onChange={(e) => { setTitle(e.target.value) }} 
-              required
-            />
-          </label>
-          <label>
-            Rating
-            <input 
-              type= "number"
-              name="rating" 
-              placeholder="enter the rating" 
-              value={rating} 
-              min={1}
-              max={10}
-              onChange={(e) => { setRating(e.target.value) }} 
-              required
-            />
-          </label>
-            Image
-            <input 
-              type= "text"
-              accept= "image"
-              name="image" 
-              placeholder="enter the image URL" 
-              value={imgURL} 
-              onChange={(e) => { setImage(e.target.value) }} 
-            />
-        <button>Create</button>
-        </form>
-      </section>
-
+      <AddMovie callbackToAddMovie={addNewMovie} />
       <Main movies={moviesToDisplay} callbackToDelete={deleteMovie} />
       <Footer />
     </>
@@ -94,3 +39,4 @@ function App() {
 }
 
 export default App
+
